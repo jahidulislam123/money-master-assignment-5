19e53a931b479141a43a6d9ac51f37aaefcaaf08
unknown <islam15-3879@diu.edu.bd>
2022-02-17 14:59:21 +0600

    added

--- js-file/script.js
+++ js-file/script.js
@@ -1,24 +1,3 @@
-document.getElementById('calculate-btn').addEventListener('click',function(){
-
-    console.log('clicked');
-    const income = document.getElementById('income-input');
-    const totalIncomeText =income.value;
-    const totalIncome =parseFloat(totalIncomeText);
-    // console.log(totalIncomeText);
-
-
-    const totalBalance =document.getElementById('total-balance');
-    const balanceText =totalBalance.innerText;
-    const balance=parseFloat(balanceText)
-
-    // console.log(balance);
-
-    totalBalance.innerText=totalIncome+balance;
-
-    // income.value='';
-    
-    
-});
 
 document.getElementById('calculate-btn').addEventListener('click',function(){
 
@@ -37,9 +16,80 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
     const expensesText =totalExpense.innerText;
     const expenses =parseFloat(expensesText);
 
-    totalExpense.innerText =totalExpenseCost+expenses;
+    totalExpense.innerText =totalExpenseCost;
     // foodCost.value='';
     // rentCost.value='';
     // clothCost.value='';
+
+
+
+
+    const income = document.getElementById('income-input');
+    const totalIncomeText =income.value;
+    const totalIncome =parseFloat(totalIncomeText);
+    // console.log(totalIncomeText);
+
+
+    const totalBalance =document.getElementById('total-balance');
+    const balanceText =totalBalance.innerText;
+    const balance=parseFloat(balanceText)
+
+    // console.log(balance);
+
+    totalBalance.innerText=totalIncome-totalExpenseCost;
+
+    // income.value='';
     
 })
+
+
+
+
+document.getElementById('save-btn').addEventListener('click',function(){
+
+    // console.log('clicked');
+    const save= document.getElementById('saving-input');
+    const saveText =parseFloat(save.value);
+    // console.log(saveText);
+    const savingVag =saveText/100;
+    
+
+    const income1 = document.getElementById('income-input');
+    const totalIncomeText1 =income1.value;
+    const totalIncome1 =parseFloat(totalIncomeText1);
+    // console.log(totalIncome1);
+
+    const percentSave =totalIncome1*savingVag;
+    
+
+
+    const savingAmount =document.getElementById('saving-amount');
+    const savingAmountText =savingAmount.innerText;
+    const savintAmountTotal =parseFloat(savingAmountText);
+    // console.log(savintAmountTotal);
+    savingAmount.innerText=percentSave;
+
+
+
+    const totalBalance =document.getElementById('total-balance');
+    const balanceText =totalBalance.innerText;
+    const balance=parseFloat(balanceText);
+    // console.log(balance);
+
+    const remainingAmountFinal = balance-percentSave;
+
+
+    const remainingAmount =document.getElementById('remaining-amount');
+    console.log(remainingAmount.innerText);
+
+    
+    // console.log(remainingAmountFinal);
+    remainingAmount.innerText=remainingAmountFinal;
+    
+
+
+    
+    
+    
+    
+});
